8ddf60c09ea514a534615ce37aee255b8bd3fad8
ybenchel <yass515bec@outlook.fr>
2025-12-15 19:07:12 +0100

    changes1

--- requirements/mariadb/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mysql_install_db
-mysqld
--- requirements/wordpress/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root
-./wp-cli.phar core install --url="$WP_SITE_URL" --title="$WP_SITE_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_EMAIL" --allow-root
-
-php-fpm8.4 -F
