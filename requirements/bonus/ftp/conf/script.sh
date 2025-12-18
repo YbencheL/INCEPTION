84abc269b90a3152c9e56c7821a50a471324e892
ybenchel <yass515bec@outlook.fr>
2025-12-18 19:39:41 +0100

    changes4

--- requirements/bonus/ftp/conf/script.sh
+++ requirements/bonus/ftp/conf/script.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-if [ ! id "$FTP_USER" >/dev/null 2>&1 ]; then
-    adduser -h /ftp -D $FTP_USER
+if  ! id "$FTP_USER" >/dev/null 2>&1 ; then
+
+    adduser --home /ftp --shell /bin/bash --disabled-password --gecos "" "$FTP_USER"
     echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
 fi
 
 exec vsftpd /etc/vsftpd.conf
